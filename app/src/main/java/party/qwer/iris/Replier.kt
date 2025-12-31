bbf7eb5c4004fd87b7056e1b3787a25c2eeeed7d
mingyu091031 <kmk0910310914@gmail.com>
2025-12-31 21:51:06 +0900

    first commit

--- app/src/main/java/party/qwer/iris/Replier.kt
+++ app/src/main/java/party/qwer/iris/Replier.kt
@@ -240,7 +240,16 @@ class Replier {
                 "mov" -> "video/quicktime"
                 "flv" -> "video/x-flv"
                 "ogv" -> "video/ogg"
-                else -> "image/*"  // 기본값
+                // 오디오
+                "mp3" -> "audio/mpeg"
+                "wav" -> "audio/wav"
+                "tta" -> "audio/x-tta"
+                "tak" -> "audio/x-tak"
+                "aac" -> "audio/aac"
+                "wma" -> "audio/x-ms-wma"
+                "ogg" -> "audio/ogg"
+                "m4a" -> "audio/mp4"
+                else -> "*/*"
             }
         }
 
